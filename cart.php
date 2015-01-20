a6d908e4ed7944664eca0ef90d9b06a343361116
Eddie Gurnee <pegurnee@gmail.com>
2015-01-19 19:29:36 -0500

    included basic pages

---
+++ cart.php
@@ -0,0 +1,8 @@
+<!doctype html>
+<html>
+  <head>
+    <title>3-B Cart</title>
+  </head>
+  <body>
+  </body>
+</html>
---
+++ search.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html>
+  <head>
+    <title>3-B Search Results</title>
+  </head>
+  <body>
+    <div id="header"></div>
+    <div id="login"></div>
+    <div id="footer"></div>
+  </body>
+</html>
