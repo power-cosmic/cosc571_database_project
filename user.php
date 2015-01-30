358df4f9809742f86b2e4cacbae28ea8a7440fd8
Tyler Hoffman <tyler.c.hoffman@gmail.com>
2015-01-30 17:01:24 -0500

    Update uesr.php includes

--- user.php
+++ user.php
@@ -1,5 +1,6 @@
 <?php
   include_once 'admin/php/common.php';
+  include_once 'admin/php/displays.php';
 ?>
 <!doctype html>
 <html>
@@ -7,5 +8,6 @@
     echo createBasicHead('3-B - User', ['test.js']);
   ?>
   <body>
+    <?php echo createHeader(); ?>
   </body>
 </html>
