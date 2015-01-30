72ff41b427d514f5f4966fbc03279341f196c446
Eddie Gurnee <pegurnee@gmail.com>
2015-01-30 15:03:25 -0500

    include header stuff

---
+++ admin/php/header.php
@@ -0,0 +1,8 @@
+<?php
+  function createBasicHead($title = '3-B.com') {
+?>
+  <title><?=$title?></title>
+  <link rel="stylesheet" type="text/css" href="<?=../css/styles.css?>">
+<?php
+  }
+?>
--- user.php
+++ user.php
@@ -1,6 +1,12 @@
+<?php
+  include_once 'admin/php/header.php';
+?>
 <!doctype html>
 <html>
   <head>
+  <?php
+    createBasicHead('3-B - User');
+  ?>
   </head>
   <body>
   </body>
