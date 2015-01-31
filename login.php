bb9bcd2c2e24cc2ac9bd4797ac4f8ecdea60ebee
Tyler Hoffman <tyler.c.hoffman@gmail.com>
2015-01-30 19:35:35 -0500

    Add form to login

--- login.php
+++ login.php
@@ -1,7 +1,27 @@
+<?php
+  include_once 'admin/php/common.php';
+  include_once 'admin/php/displays.php';
+?>
 <!doctype html>
 <html>
-  <head>
-  </head>
+  <?=createBasicHead('3-B - Login')?>
   <body>
+    <?=createHeader()?>
+    <div id="login">
+      
+      <form method="post">
+        <label for="username">username</label>
+        <input type="text" name="username" id="username" placeholder="username">
+        <br>
+        
+        <label for="password">password</label>
+        <input type="password" name="password" id="password">
+        <br>
+        
+        <input type="submit" value="login">
+      </form>
+      
+    </div>
+    <?=createFooter()?>
   </body>
 </html>
