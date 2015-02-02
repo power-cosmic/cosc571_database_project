a2f4409db66a8f018a7f9e238d8e83e6b73fcc93
Tyler Hoffman <tyler.c.hoffman@gmail.com>
2015-02-02 09:26:53 -0500

    Clean up login.php

--- login.php
+++ login.php
@@ -12,14 +12,10 @@ include_once 'admin/php/displays.php';
       <div id="login" class="centered box">
 
         <form method="post">
-          <input type="text" name="username" id="username" placeholder="username"> <input type="password"
-            name="password" id="password" placeholder="password"> <input
-            type="submit" value="login" class="blue button">
+          <input type="text" name="username" id="username" placeholder="username">
+          <input type="password" name="password" id="password" placeholder="password">
+          <input type="submit" value="login" class="blue button">
         </form>
-        <br>
-        <a href="login.php" class="green big-button">shop</a>
-        <a href="login.php" class="blue big-button">buy</a>
-        <a href="login.php" class="purple big-button">explore</a>
       </div>
     </div>
     <?=createFooter()?>
