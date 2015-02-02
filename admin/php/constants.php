dfcec5db1d382de68d6fba1ea7c7de49a3545cf9
Tyler Hoffman <tyler.c.hoffman@gmail.com>
2015-02-02 10:17:05 -0500

    Add possible fix for constants.php

--- admin/php/constants.php
+++ admin/php/constants.php
@@ -3,12 +3,16 @@
       'admin'=>'admin',
       'user'=>'user'
   );
+ 
+  $project_root = explode('/', $_SERVER['SCRIPT_NAME'])[0];
+  if (strlen($project_root)) {
+    $project_root.="/";
+  }
   
-  $project_root = explode('/admin/', $_SERVER['SCRIPT_NAME'])[0];
+  $admin_root = $project_root."admin/";
   
   $locations = array(
-      'main_style'=> $project_root.'css/styles.css',
+      'main_style'=> $admin_root.'css/styles.css',
       'home'=> $project_root
   );
-  
 ?>
