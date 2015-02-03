b2abbb08614aba1bf3aa20cf484c8395d5d92998
Eddie Gurnee <pegurnee@gmail.com>
2015-02-03 14:07:27 -0500

    login page refactored

--- login.php
+++ login.php
@@ -1,22 +1,19 @@
 <?php
-include_once 'admin/php/common.php';
-include_once 'admin/php/displays.php';
+  include_once 'admin/php/common.php';
+  include_once 'admin/php/displays.php';
 ?>
 <!doctype html>
 <html>
-  <?=createBasicHead('3-B | Login')?>
+  <?=createBasicHead('Login')?>
   <body>
     <div id="container">
       <?=createHeader(true, false)?>
-
       <div class="content">
         <div id="login" class="centered box">
-  
           <form method="post" action="<?=$_SERVER['PHP_SELF']?>">
-            <input type="text" name="username" id="username"
-              placeholder="username"> <input type="password"
-              name="password" id="password" placeholder="password"> <input
-              type="submit" value="login" class="blue button">
+            <input type="text" name="username" id="username" placeholder="username">
+            <input type="password" name="password" id="password" placeholder="password">
+            <input type="submit" value="login" class="blue button">
           </form>
         </div>
       </div>
