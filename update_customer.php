0ddc006ac19ece5fe20a0f308a9308cce24f1d47
Tyler Hoffman <tyler.c.hoffman@gmail.com>
2015-02-05 21:27:14 -0500

    Add update_customer page

---
+++ update_customer.php
@@ -0,0 +1,25 @@
+<?php
+  session_start();
+  include_once 'admin/php/constants.php';
+  include_once 'admin/php/common.php';
+  include_once 'admin/php/displays.php';
+  include_once 'admin/php/profile.php';
+  include_once 'admin/php/us_state_dropdown.php';
+  
+  $username = ($_SESSION['username'])? $_SESSION['username']: 'unknown';
+?>
+<!doctype html>
+<html>
+  <?=createBasicHead('Update')?>
+  <body>
+    <div id="container">
+      <?=createHeader(true, false)?>
+      <div class="content">
+        <div id="login" class="centered box">
+          <?=createProfileForm($username)?>
+        </div>
+      </div>
+      <?=createFooter()?>
+    </div>
+  </body>
+</html>
