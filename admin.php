71495d6286dcb8b3d9deca9c9e5e6c350fe60650
Eddie Gurnee <pegurnee@gmail.com>
2015-02-06 15:21:14 -0500

    Included constansts to admin to allow for actual comparision.

--- admin.php
+++ admin.php
@@ -2,6 +2,7 @@
 include_once 'admin/php/common.php';
 include_once 'admin/php/displays.php';
 include_once 'admin/php/admin_login.php';
+include_once 'admin/php/constants.php';
 ?>
 <!doctype html>
 <html>
@@ -10,80 +11,69 @@ include_once 'admin/php/admin_login.php';
   <div id="container">
     <?=createHeader()?>
     <div class="content">
-      
+
       <? if ($_SESSION['logged_in'] == $GLOBALS['user_status']['admin']) { ?>
         <!-- display admin page -->
-        <div id="login" class="centered box">
-          
-          <p>
-            <!-- TODO: generate this w/ php -->
-            Registered users: 444
-          </p>
-          
-          <table id="books-by-category">
-            <tr>
-              <th>Category</th>
-              <th>Number of books</th>
-            </tr>
-            
-            <!-- TODO: generate this w/ php -->
-            <tr>
-              <td>Category 1</td>
-              <td>555</td>
-            </tr>
-            <tr>
-              <td>Category 2</td>
-              <td>234</td>
-            </tr>
-            
-          </table>
-          
-          <br>
-          
-          <table id="average-sales-by-month">
-            <tr>
-              <th>Month</th>
-              <th>Average Monthly Sales</th>
-            </tr>
-            
-            <!-- TODO: generate this w/ php -->
-            <tr>
-              <td>January</td>
-              <td>$555</td>
-            </tr>
-            <tr>
-              <td>February</td>
-              <td>$234</td>
-            </tr>
-            
-          </table>
-          
-          <br>
-          
-          <table id="book-table">
-            <tr>
-              <th>Title</th>
-              <th>Number Reviews</th>
-            </tr>
-            
-            <!-- TODO: generate this w/ php -->
-            <tr>
-              <td>Book 1</td>
-              <td>654</td>
-            </tr>
-            <tr>
-              <td>Book 2</td>
-              <td>13</td>
-            </tr>
-            
-          </table>
-          
-        </div>
-      
-      <? } else { 
+      <div id="login" class="centered box">
+        <p>
+          <!-- TODO: generate this w/ php -->
+          Registered users: 444
+        </p>
+        <table id="books-by-category">
+          <tr>
+            <th>Category</th>
+            <th>Number of books</th>
+          </tr>
+          <!-- TODO: generate this w/ php -->
+          <tr>
+            <td>Category 1</td>
+            <td>555</td>
+          </tr>
+          <tr>
+            <td>Category 2</td>
+            <td>234</td>
+          </tr>
+        </table>
+        <br>
+        <table id="average-sales-by-month">
+          <tr>
+            <th>Month</th>
+            <th>Average Monthly Sales</th>
+          </tr>
+          <!-- TODO: generate this w/ php -->
+          <tr>
+            <td>January</td>
+            <td>$555</td>
+          </tr>
+          <tr>
+            <td>February</td>
+            <td>$234</td>
+          </tr>
+        </table>
+        <br>
+        <table id="book-table">
+          <tr>
+            <th>Title</th>
+            <th>Number Reviews</th>
+          </tr>
+          <!-- TODO: generate this w/ php -->
+          <tr>
+            <td>Book 1</td>
+            <td>654</td>
+          </tr>
+          <tr>
+            <td>Book 2</td>
+            <td>13</td>
+          </tr>
+        </table>
+      </div>
+
+      <?
+      } else {
         echo generate_admin_login();
-      } ?>
-      
+      }
+      ?>
+
     </div>
     <?=createFooter()?>
     </div>
