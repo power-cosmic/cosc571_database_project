83af90f03d398bbde4b0274fa20556cf601bf807
Eddie Gurnee <pegurnee@gmail.com>
2015-02-06 13:47:15 -0500

    formatting for head to match html

--- admin/php/common.php
+++ admin/php/common.php
@@ -8,25 +8,27 @@
     <link rel="stylesheet" type="text/css" href="' . $GLOBALS['locations']['main_style'] . '">';
     
     foreach($styles as $style) {
-      $toReturn .= '<link rel="stylesheet" type="text/css" href="' . $GLOBALS['locations']['styles'] . $style . '">';
+      $toReturn .= '
+    <link rel="stylesheet" type="text/css" href="' . $GLOBALS['locations']['styles'] . $style . '">';
     }
     
     if ($script_location) {
-      $toReturn .= '<script src="' . $GLOBALS['locations']['lib'] . 'require.js"></script>
-          <script>
-            require.config({
-              paths: {
-                lib: "' . $GLOBALS['admin_root'] . 'lib",
-                js: "' . $GLOBALS['admin_root'] . 'js"
-              }
-            });
-            require(["js/' . $script_location . '"]);
-            </script>';
+      $toReturn .= '
+    <script src="' . $GLOBALS['locations']['lib'] . 'require.js"></script>
+    <script>
+      require.config({
+        paths: {
+          lib: "' . $GLOBALS['admin_root'] . 'lib",
+          js: "' . $GLOBALS['admin_root'] . 'js"
+        }
+      });
+      require(["js/' . $script_location . '"]);
+    </script>';
     }
 
-    $toReturn .= '</head>';
+    $toReturn .= '
+  </head>' . "\n";
 
     return $toReturn;
   }
-  
 ?>
