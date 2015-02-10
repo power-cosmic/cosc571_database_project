c0dce1cbc591509dad6c200bbfff304fe2f59e25
Eddie Gurnee <pegurnee@gmail.com>
2015-02-10 13:50:54 -0500

    Getting the logout page to work

--- admin/php/constants.php
+++ admin/php/constants.php
@@ -1,30 +1,31 @@
-<?php 
-  $user_status = array(
-      'admin' => 'admin',
-      'user' => 'user'
-  );
+<?php
+$user_status = array(
+  'admin' => 'admin',
+  'user' => 'user'
+);
 
-  $name = array(
-      'long' => 'Best-Buy-Books',
-      'short' => '3-B'
-  );
- 
-  $project_root = explode('/', $_SERVER['SCRIPT_NAME'])[0];
-  if (strlen($project_root)) {
-    $project_root .= "/";
-  }
-  
-  $admin_root = $project_root . "admin/";
-  
-  $locations = array(
-      'styles' => $admin_root . 'css/',
-      'main_style' => $admin_root . 'css/styles.css',
-      'scripts' => $admin_root . 'js/',
-      'lib' => $admin_root . 'lib/',
-      'home' => $project_root . 'index.php',
-      'cart' => $project_root . 'cart.php',
-      'admin' => $project_root . 'admin.php',
-      'register' => $project_root . 'register.php',
-      'login' => $project_root . 'login.php'
-  );
+$name = array(
+  'long' => 'Best-Buy-Books',
+  'short' => '3-B'
+);
+
+$project_root = explode('/', $_SERVER['SCRIPT_NAME'])[0];
+if (strlen($project_root)) {
+  $project_root .= "/";
+}
+
+$admin_root = $project_root . "admin/";
+
+$locations = array(
+  'styles' => $admin_root . 'css/',
+  'main_style' => $admin_root . 'css/styles.css',
+  'scripts' => $admin_root . 'js/',
+  'lib' => $admin_root . 'lib/',
+  'home' => $project_root . 'index.php',
+  'cart' => $project_root . 'cart.php',
+  'admin' => $project_root . 'admin.php',
+  'register' => $project_root . 'register.php',
+  'login' => $project_root . 'login.php',
+  'logout' => $admin_root . 'php/logout.php'
+);
 ?>
--- admin/php/displays.php
+++ admin/php/displays.php
@@ -15,7 +15,7 @@ function createHeader($showCart = true, $showLogin = true) {
       $to_return .= '
             <div id="logInButton">
               <a href="' . $GLOBALS['locations']['login'] . '" class="glow-link">log in</a> |
-              <a href="' . $GLOBALS['locations']['register'] . '" class="glow-link">register</a> | 
+              <a href="' . $GLOBALS['locations']['register'] . '" class="glow-link">register</a> |
               <a href="' . $GLOBALS['locations']['cart'] . '" class="glow-link">cart</a>
             </div>';
     }
@@ -24,7 +24,7 @@ function createHeader($showCart = true, $showLogin = true) {
       case $GLOBALS['user_status']['admin']:
         $to_return .= '
             <div id="adminLogoutButton">
-              <a href="logout.php" class="glow-link">logout</a>
+              <a href="' . $GLOBALS['locations']['logout'] . '" class="glow-link">logout</a>
             </div>';
         break;
       case $GLOBALS['user_status']['user']:
