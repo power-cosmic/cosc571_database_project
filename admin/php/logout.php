d4e38ef6e3a01711a5989477ded792cf90feede0
Eddie Gurnee <pegurnee@gmail.com>
2015-02-10 15:07:25 -0500

    added die to logout, its not working

--- admin/php/logout.php
+++ admin/php/logout.php
@@ -1,9 +1,9 @@
-<?php 
-  session_start();
-  session_unset();
-  
-  include_once 'constants.php';
-  
-  header('Location: ' . $GLOBALS['locations']['home']);
+<?php
+session_start();
+session_unset();
 
+include_once 'constants.php';
+
+header('Location: ' . $GLOBALS['locations']['home']);
+die();
 ?>
