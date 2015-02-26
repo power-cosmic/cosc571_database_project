3efd7a1cc9f188ef8ee4d2cc2fd83846c0f3e547
Tyler Hoffman <tyler.c.hoffman@gmail.com>
2015-02-26 16:03:56 -0500

    Add create_book

---
+++ admin/sql/create_tables/create_book.sql
@@ -0,0 +1,14 @@
+CREATE TABLE book
+(
+	isbn INT(20) UNSIGNED PRIMARY KEY,
+	title VARCHAR(256) NOT NULL,
+	description VARCHAR(2048),
+	price NUMERIC(6, 2) NOT NULL,
+	author_id INT(11) UNSIGNED NOT NULL,
+	genre_id INT(11) UNSIGNED NOT NULL,
+	publisher_id INT(11) UNSIGNED NOT NULL,
+	
+	FOREIGN KEY (author_id) REFERENCES author (id),
+	FOREIGN KEY (genre_id) REFERENCES genre (id),
+	FOREIGN KEY (publisher_id) REFERENCES publisher (id)
+);
