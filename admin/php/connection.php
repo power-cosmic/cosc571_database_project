1cacabc2eb5a7555a8876d89ac7e509d4239d68c
Tyler Hoffman <tyler.c.hoffman@gmail.com>
2015-02-27 10:17:05 -0500

    Fix optional parameter on connection.php

--- admin/php/connection.php
+++ admin/php/connection.php
@@ -1,9 +1,9 @@
 <?php
-function open_connection($table = 'dbname=bbb_te') {
-  $db = new PDO('mysql:host=localhost;' . $table . ';charset=utf8', 'u201501_471_g04', 'passwd');
+function open_connection($table = 'bbb_te') {
+  $db = new PDO('mysql:host=localhost;dbname=' . $table . ';charset=utf8', 'u201501_471_g04', 'passwd');
   $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
   $db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
-return $db;
+  return $db;
 }
 
 ?>
