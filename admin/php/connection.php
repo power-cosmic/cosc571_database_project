2fc3a0ddc61a6adf058425e90e6464f413d47d9d
Tyler Hoffman <tyler.c.hoffman@gmail.com>
2015-02-27 10:06:23 -0500

    Add optional database selection in connection.php

--- admin/php/connection.php
+++ admin/php/connection.php
@@ -1,6 +1,6 @@
 <?php
-function open_connection() {
-  $db = new PDO('mysql:host=localhost;dbname=bbb_te;charset=utf8', 'u201501_471_g04', 'passwd');
+function open_connection($table = 'dbname=bbb_te') {
+  $db = new PDO('mysql:host=localhost;' . $table . ';charset=utf8', 'u201501_471_g04', 'passwd');
   $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
   $db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
 return $db;
