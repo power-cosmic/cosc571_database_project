264a8aaec62dafd0826b61242de157a46c5ac6a3
Tyler Hoffman <tyler.c.hoffman@gmail.com>
2015-03-06 21:09:46 -0500

    Get rid of old stuff in book_info.php

--- admin/php/book_info.php
+++ admin/php/book_info.php
@@ -18,92 +18,57 @@ class Book {
     }
   }
   
+  /**
+   * Generate content for a cell in tables holding book information.
+   * For use in Cart, Order-confirmation, etc.
+   *
+   * @param boolean $show_publisher Whether or not to show publisher info
+   * @param boolean $show_isbn Whether or not to show isbn
+   */
   public function generateBookInfo($show_publisher = false, $showISBN = false) {
 
-    $to_return = generateBookInfoLine('Title', $this->title)
-    . generateBookInfoLine('Author', $this->first_name . ' ' . $this->last_name)
-    . generateBookInfoLine('Price', "$" . $this->price);
+    $to_return = $this->generateBookInfoLine('Title', $this->title)
+    . $this->generateBookInfoLine('Author', $this->first_name . ' ' . $this->last_name)
+    . $this->generateBookInfoLine('Price', "$" . $this->price);
     
     if ($show_publisher) {
-      $to_return .= generateBookInfoLine('Publisher', $this->publisher);
+      $to_return .= $this->generateBookInfoLine('Publisher', $this->publisher);
     }
     
     if ($showISBN) {
-      $to_return .= generateBookInfoLine('ISBN', $this->isbn);
+      $to_return .= $this->generateBookInfoLine('ISBN', $this->isbn);
     }
     
     return $to_return;
   }
   
+  /**
+   * Generate line in book info content
+   *
+   * @param string $label
+   *          Label (one word only)
+   * @param string $content
+   *          Content
+   */
   private static function generateBookInfoLine($label, $content) {
     $lower_case = strtolower($label);
     return '<div class="book-info book-' . $lower_case . '">
           <label class="book-label">' . $label . '</label>' . $content . '</div>';
   }
   
-  public function generateBookView($book) {
+  /**
+   * Generate content for a book with cover, used on the main page
+   */
+  public function generateBookView() {
     $to_return = '<div class="book-view">
           <img src="' . $cover . '" class="book-cover" /><div class="book-info-box">'
-            . generateBookInfoLine('Title', $this->title)
-            . generateBookInfoLine('Author', $this->first_name . ' ' . $this->last_name)
+            . $this->generateBookInfoLine('Title', $this->title)
+            . $this->generateBookInfoLine('Author', $this->first_name . ' ' . $this->last_name)
             . '</div>
              </div>';
     return $to_return;
   }
   
-}
-
-/**
- * Generate content for a cell in tables holding book information.
- * For use in Cart, Order-confirmation, etc.
- *
- * @param array   $book Book information (title, author, etc)
- * @param boolean $show_publisher Whether or not to show publisher info
- * @param boolean $show_isbn Whether or not to show isbn
- */
-function generateBookInfo($book, $show_publisher = false, $showISBN = false) {
-  $to_return = generateBookInfoLine('Title', $book['title'])
-    . generateBookInfoLine('Author', $book['first_name'] . ' ' . $book['last_name'])
-    . generateBookInfoLine('Price', "$" . $book['price']);
-
-  if ($show_publisher) {
-    $to_return .= generateBookInfoLine('Publisher', $book['publisher']);
-  }
-
-  if ($showISBN) {
-    $to_return .= generateBookInfoLine('ISBN', $book['isbn']);
-  }
-
-  return $to_return;
-}
-
-/**
- * Generate line in book info content
- *
- * @param string $label
- *          Label (one word only)
- * @param string $content
- *          Content
- */
-function generateBookInfoLine($label, $content) {
-  $lower_case = strtolower($label);
-  return '<div class="book-info book-' . $lower_case . '">
-        <label class="book-label">' . $label . '</label>' . $content . '</div>';
-}
-
-/**
- * Generate content for a book with cover, used on the main page
- *
- * @param array   $book Book information (title, author, etc)
- */
-function generateBookView($book) {
-  $to_return = '<div class="book-view">
-        <img src="' . $book['cover'] . '" class="book-cover" /><div class="book-info-box">'
-          . generateBookInfoLine('Title', $book['title'])
-          . generateBookInfoLine('Author', $book['first_name'] . ' ' . $book['last_name'])
-          . '</div>
-           </div>';
-  return $to_return;
 }
 
 ?>
