f77f158df6a98c50b040a10c4a49f415caedf79d
Eddie Gurnee <pegurnee@gmail.com>
2015-03-09 23:22:11 -0400

    isbn_check_digit generation function working

---
+++ admin/sql/functions/isbn_check_digit.sql
@@ -0,0 +1,29 @@
+DROP FUNCTION IF EXISTS isbn_check_digit;
+DELIMITER //
+
+CREATE FUNCTION isbn_check_digit(isbn_check CHAR(12))
+    RETURNS CHAR(1)
+    BEGIN
+        DECLARE check_digit INT;
+        DECLARE temp_num INT;
+        DECLARE loop_index INT;
+        SET check_digit = 0;
+        SET loop_index = 0;
+        
+		count_loop: LOOP
+			SET loop_index = loop_index + 1;
+			IF (loop_index % 2) = 1 THEN
+				SET temp_num = CAST(SUBSTRING(isbn_check, loop_index, 1) AS UNSIGNED) * 1;
+			ELSE 
+				SET temp_num = CAST(SUBSTRING(isbn_check, loop_index, 1) AS UNSIGNED) * 3;
+			END IF;
+			SET check_digit = check_digit + temp_num;
+			IF loop_index = CHAR_LENGTH(isbn_check) THEN
+				LEAVE count_loop;
+			END IF;
+		END LOOP;
+		SET check_digit = 10 - check_digit % 10;
+        RETURN CAST(check_digit AS CHAR(1));
+    END //
+
+DELIMITER ;
