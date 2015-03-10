baafead301c03735476a5ea2c70fcda8d116a71f
Eddie Gurnee <pegurnee@gmail.com>
2015-03-10 00:40:30 -0400

    isbn correctly generating for inserted book

--- admin/sql/triggers/on_insert_book.sql
+++ admin/sql/triggers/on_insert_book.sql
@@ -1,6 +1,14 @@
 DROP TRIGGER IF EXISTS update_publisher_book;
+DELIMITER $$
 CREATE TRIGGER update_publisher_book
-	AFTER INSERT
+	BEFORE INSERT
 	ON book
 	FOR EACH ROW
-	UPDATE publisher SET num_books=num_books+1 WHERE NEW.publisher_id=publisher.id;
+	BEGIN
+		UPDATE publisher 
+			SET num_books=num_books+1, 
+				NEW.isbn=isbn_with_check_digit(CONCAT('9780', code, LPAD(num_books, 8 - CHAR_LENGTH(code), '0')))
+			WHERE publisher.id=NEW.publisher_id;
+	END $$
+
+DELIMITER ;
