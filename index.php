7776edbaba7b66f246dd0e1d823fabaf1bb47a63
Eddie Gurnee <pegurnee@gmail.com>
2015-03-11 01:05:11 -0400

    add ten least expensive books to index

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
 include_once 'admin/php/common.php';
 include_once 'admin/php/displays.php';
+include_once 'admin/php/connection.php';
 include_once 'admin/php/book_info.php';
 
 session_start();
@@ -17,22 +18,23 @@ session_start();
         ?>
         <h2 class="book-view-header">Best Sellers:</h2>
         <div id="top_picks" class="scrolling-view">
+          <span class="v-align-helper"></span>
           <?php
           //get the top 10 number of sold books
           $book_info = [
               'id' => 1,
               'title' => 'Absolute Java Is The Best Book Around',
-              'author' => 'Walter Savitch',
+              'first_name' => 'Walter',
+              'last_name' => 'Savitch',
               'price' => '149.99',
               'quantity' => '2',
               'publisher' => 'Addison-Wesley',
               'isbn' => '978-0132834230',
               'cover' => 'admin/images/icons/cart.jpg'
           ];
-          
+
           $book = new Book($book_info);
 
-          echo '<span class="v-align-helper"></span>';
           for ($i = 0; $i < 10; $i++) {
             echo $book->generateBookView();
           }
@@ -41,14 +43,24 @@ session_start();
         </div>
         <h2 class="book-view-header">Hot Buys:</h2>
         <div id="recent_picks" class="scrolling-view">
+          <span class="v-align-helper"></span>
           <?php
             //get the top 10 most recently purchased books
           ?>
         </div>
         <h2 class="book-view-header">Great Deals:</h2>
         <div id="great_deals" class="scrolling-view">
+          <span class="v-align-helper"></span>
           <?php
             //get the top 10 least expensive books
+            $sql = "SELECT isbn,title,price,first_name,last_name FROM book,author WHERE book.author_id=author.id GROUP BY price LIMIT 10;";
+            $db = open_connection();
+            $stmt = $db->prepare($sql);
+            $stmt->execute();
+            while($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+              $book = new Book($row);
+              echo $book->generateBookView();
+            }
           ?>
         </div>
       </div>
