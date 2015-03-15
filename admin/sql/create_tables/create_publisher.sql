4a478e46e9e3cdcc64f26814e1cf9130f3f45b47
Eddie Gurnee <pegurnee@gmail.com>
2015-03-14 22:32:31 -0400

    added publisher code, num_books for isbn generation

--- admin/sql/create_tables/create_publisher.sql
+++ admin/sql/create_tables/create_publisher.sql
@@ -1,5 +1,7 @@
 CREATE TABLE publisher
 (
 	id INT(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-	name varchar(64) NOT NULL
+	name varchar(64) NOT NULL,
+  code INT(7),
+  num_books INT(10) UNSIGNED NOT NULL DEFAULT 0
 );
