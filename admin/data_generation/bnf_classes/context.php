419f7b9c3934ab1ced12653ea05af8aec752f98f
Tyler Hoffman <tyler.c.hoffman@gmail.com>
2015-03-17 00:00:07 -0400

    Add I

--- admin/data_generation/bnf_classes/context.php
+++ admin/data_generation/bnf_classes/context.php
@@ -3,10 +3,12 @@
 class Context {
   public $is_plural;
   public $is_specific;
+  public $is_subject;
   
   function __construct() {
     $this->is_plural = false;
     $this->is_specific = false;
+    $this->is_subject = false;
   }
 }
 
---
+++ admin/data_generation/bnf_classes/i.php
@@ -0,0 +1,27 @@
+<?php
+require_once 'noun.php';
+
+class I extends Noun {
+  
+  function __construct() {
+    $data = [
+      'base' => 'I',
+      'plural' => 'we',
+      'is_proper' => true
+    ];
+    parent::__construct($data);
+  }
+  
+  public function evaluate($context) {
+    if ($context->is_subject) {
+      return parent::evaluate($context);
+    } elseif ($context->is_plural) {
+      return 'us';
+    } else {
+      return 'me';
+    }
+  }
+  
+}
+
+?>
