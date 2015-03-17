47feb878aaa41cef7e3a4266b59a87bea11a439a
Tyler Hoffman <tyler.c.hoffman@gmail.com>
2015-03-17 00:30:05 -0400

    Add subject to context

--- admin/data_generation/bnf_classes/context.php
+++ admin/data_generation/bnf_classes/context.php
@@ -1,14 +1,20 @@
 <?php
 
 class Context {
+  
+  public $subject;
   public $is_plural;
   public $is_specific;
   public $is_subject;
+  public $is_past;
+  
   
   function __construct() {
+    $this->subject = null;
     $this->is_plural = false;
     $this->is_specific = false;
     $this->is_subject = false;
+    $this->is_past = false;
   }
 }
 
