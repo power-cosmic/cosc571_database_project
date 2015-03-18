c4bbb3eaa728a2c54bde103af84854e326a804b3
Tyler Hoffman <tyler.c.hoffman@gmail.com>
2015-03-17 22:24:29 -0400

    Add quantity to order_item

--- admin/sql/create_tables/create_order_item.sql
+++ admin/sql/create_tables/create_order_item.sql
@@ -1,6 +1,7 @@
 CREATE TABLE IF NOT EXISTS order_item
 ( order_id INT(11) UNSIGNED
 , book_isbn CHAR(13)
+, quantity INT(4) NOT NULL
 
 , FOREIGN KEY (book_isbn) REFERENCES book (isbn)
 , FOREIGN KEY (order_id) REFERENCES sales_order (id)
