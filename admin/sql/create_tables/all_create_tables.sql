1ceedcd5a279bb734616f6d1be3e2be61f7a5f01
Eddie Gurnee <pegurnee@gmail.com>
2015-03-18 11:57:56 -0400

    all create and all drop tables updated

--- admin/sql/create_tables/all_create_tables.sql
+++ admin/sql/create_tables/all_create_tables.sql
@@ -3,20 +3,22 @@ set FOREIGN_KEY_CHECKS=0;
 \. create_address.sql
 \. create_admin.sql
 \. create_author.sql
-\. create_book.sql
 \. create_book_genre.sql
+\. create_book.sql
 \. create_coupon.sql
 \. create_credit_card.sql
-\. create_customer.sql
 \. create_customer_address.sql
+\. create_customer.sql
 \. create_genre.sql
-\. create_order.sql
 \. create_order_item.sql
+\. create_order.sql
 \. create_phone.sql
 \. create_publisher.sql
 \. create_review.sql
-\. create_series.sql
 \. create_series_entry.sql
+\. create_series.sql
+\. create_shipping_zone.sql
+\. create_state.sql
 \. create_wishlist.sql
 
 set FOREIGN_KEY_CHECKS=1;
