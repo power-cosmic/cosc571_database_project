cc0bf88f44d0951a710ab8a7c63050a95ba08de8
Tyler Hoffman <tyler.c.hoffman@gmail.com>
2015-03-20 23:37:34 -0400

    Add insert adjectives

---
+++ admin/data_generation/sql/insert_statements/adjectives.sql
@@ -0,0 +1,6 @@
+INSERT INTO adjectives (base) VALUES ('great');
+INSERT INTO adjectives (base) VALUES ('horrible');
+INSERT INTO adjectives (base) VALUES ('boring');
+INSERT INTO adjectives (base) VALUES ('sleazy');
+INSERT INTO adjectives (base) VALUES ('trixie');
+INSERT INTO adjectives (base) VALUES ('best');
