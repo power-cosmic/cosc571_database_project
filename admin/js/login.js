4384306d254a44faf01d13e917d409c068e65f0f
Tyler Hoffman <tyler.c.hoffman@gmail.com>
2015-03-30 23:46:56 -0400

    Make login generic; customerLogin specifies customer login action

--- admin/js/login.js
+++ admin/js/login.js
@@ -7,30 +7,31 @@ define([
         	'lib/jquery'
         ], function(formUtils, ErrorAdder) {
 	
-	errorAdder = new ErrorAdder('#errors');
-	
-	$('form').submit(function(e) {
-		e.preventDefault();
+	return function Login(action) {
+		errorAdder = new ErrorAdder('#errors');
 		
-		var data = formUtils.extractData('form');
-		
-		$.ajax({
-			url: 'admin/php/handlers/login_handler.php',
-			dataType: 'json',
-			method: 'post',
-			data: $.extend(data, {
-				action: 'customer_login' 
-			}),
-			success: function(response) {
-				if (response.status == 'success') {
-					window.location.href = 'index.php';
-				} else {
-					errorAdder.showError(response.message);
-					console.log(errorAdder);
-					console.log('Failure: ', response);
+		$('form').submit(function(e) {
+			e.preventDefault();
+			
+			var data = formUtils.extractData('form');
+			
+			$.ajax({
+				url: 'admin/php/handlers/login_handler.php',
+				dataType: 'json',
+				method: 'post',
+				data: $.extend(data, {
+					action: action 
+				}),
+				success: function(response) {
+					if (response.status == 'success') {
+						window.location.href = 'index.php';
+					} else {
+						errorAdder.showError(response.message);
+						console.log(errorAdder);
+						console.log('Failure: ', response);
+					}
 				}
-			}
+			});
 		});
-	});
-	
+	};
 });
