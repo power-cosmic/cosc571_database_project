b3008f2addc04d1794825ef67cd84bc0aefc1f54
Eddie Gurnee <pegurnee@gmail.com>
2015-04-01 20:31:35 -0400

    review page fully working

--- review.php
+++ review.php
@@ -6,6 +6,27 @@ include_once 'admin/php/connection.php';
 include_once 'admin/php/review_info.php';
 
 session_start();
+
+if (isset($_POST['rating'])) {
+  $values = array(
+      'customer_username' => $_SESSION['login']->get_username(),
+      'book_isbn' => $_GET['isbn'],
+      'rating' => $_POST['rating'],
+      'content' => $_POST['content']
+  );
+
+  $db = open_connection();
+  try {
+  $sql = "INSERT INTO reveiw
+            (book_isbn, customer_username, rating, content)
+            VALUES
+            (:book_isbn, :customer_username, :rating, :content)";
+  $stmt = $db->prepare($sql);
+  $stmt->execute($values);
+  } catch (PDOException $e) {
+
+  }
+}
 ?>
 <!doctype html>
 <html>
@@ -15,7 +36,9 @@ session_start();
       <?=createHeader()?>
       <div id="content">
         <?php
-            $db = open_connection();
+            if (!$db) {
+              $db = open_connection();
+            }
             $sql = "SELECT title, CONCAT(first_name, ' ', last_name) as author
                       FROM book,author
                       WHERE isbn='" . $_GET['isbn'] . "' AND book.author_id=author.id";
@@ -24,11 +47,54 @@ session_start();
             $book = $stmt->fetch(PDO::FETCH_ASSOC);
             ?>
             <div id="cart" class="centered box">
-              <div style="position:relative">
-                <h3>Reviews for <?= $book['title']?> by <?= $book['author']?></h3>
-                <input type="submit" style="position:absolute;right:0px;top:0px;"
+              <div style="position:relative;" class="wide">
+                <h3 style="text-align:left;">Reviews for <?= $book['title']?> by <?= $book['author']?></h3>
+                <input type="button" style="position:absolute;right:0px;top:0px;"
                     class="purple button centered-input"
-                    value="Write review" name="review <?=$book->isbn?>">
+                    value="Write review" name="review <?=$book->isbn?>" onclick="showReviewForm(this);">
+
+                    <script>
+                    var numStarsSelected = 0;
+
+                    function setStars(num) {
+                    	  var stars = document.getElementsByClassName('stars');
+                    	  var limit = stars.length;
+                    	  for (var i = 0; i < limit; i++) {
+                        	  if (i < num) {
+                        		  stars[i].innerHTML = '&#x2605;';
+                        	  } else {
+                        		  stars[i].innerHTML = '&#x2606;';
+                        	  }
+                    	  }
+                    }
+                    function assignStarValue(num) {
+                    	  setStars(num);
+                    	  numStarsSelected = num;
+                    	  document.getElementById('rating-elem').value = num;
+                    }
+                    function showReviewForm(elem) {
+                    	  document.getElementById('review-form').hidden = '';
+                    	  elem.disabled = 'disabled';
+                    }
+                    </script>
+                <form id="review-form" action="<?= ($_SERVER['PHP_SELF'] . '?isbn=' . $_GET['isbn']) ?>" method="post" hidden="hidden">
+                  <div style="width:80%;cursor:pointer;" class="centered" onmouseout="setStars(numStarsSelected);">
+                    Rating:
+                    <input id="rating-elem" hidden="hidden" value="0" name="rating" />
+                    <input hidden="hidden" value="<?= $_GET['isbn'] ?>" name="isbn" />
+                    <span class="stars" onmouseover="setStars(1);" onclick="assignStarValue(1);"></span>
+                    <span class="stars" onmouseover="setStars(2);" onclick="assignStarValue(2);"></span>
+                    <span class="stars" onmouseover="setStars(3);" onclick="assignStarValue(3);"></span>
+                    <span class="stars" onmouseover="setStars(4);" onclick="assignStarValue(4);"></span>
+                    <span class="stars" onmouseover="setStars(5);" onclick="assignStarValue(5);"></span>
+
+                    <script>setStars(0);</script>
+                  </div>
+                  <textarea style="position:relative;width:80%;display:block;margin:auto;margin-bottom:5px;"
+                      rows="4" placeholder="Write your review here" name="content"></textarea>
+                  <input type="submit" class="green button centered-input"
+                      style="display:block;float:right;clear:both;margin-bottom:5px;">
+                </form>
               </div>
 
             <?php
@@ -59,7 +125,7 @@ session_start();
             <?php
             } else {
             ?>
-              <header>Oops, there's no reviews for this book! Why don't you write one?!</header>
+              <div class="wide" style="clear:both;">Oops, there's no reviews for this book! Why don't you write one?</div>
             <?php
             }
             ?>
