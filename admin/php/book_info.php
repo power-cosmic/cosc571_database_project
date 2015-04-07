b30b91e215d6b052e59f15776679b0a0173ec33b
Eddie Gurnee <pegurnee@gmail.com>
2015-04-06 23:03:49 -0400

    covers working where needed

--- admin/php/book_info.php
+++ admin/php/book_info.php
@@ -11,13 +11,13 @@ class Book {
   public $isbn = 0;
   public $description = '';
   public $cover = NULL;
-  
+
   function __construct($data) {
     foreach ($data as $key => $value) {
       $this->$key = $value;
     }
   }
-  
+
   public static function get_book($isbn) {
     $db = open_connection();
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
@@ -31,11 +31,11 @@ class Book {
     $stmt->execute(['isbn' => $isbn]);
     if ($result = $stmt->fetch(PDO::FETCH_ASSOC)) {
       $output = new Book($result);
-      
+
       return $output;
     }
   }
-  
+
   /**
    * Generate content for a cell in tables holding book information.
    * For use in Cart, Order-confirmation, etc.
@@ -48,18 +48,18 @@ class Book {
     $to_return = $this->generateBookInfoLine('Title', $this->title)
     . $this->generateBookInfoLine('Author', $this->first_name . ' ' . $this->last_name)
     . $this->generateBookInfoLine('Price', "$" . $this->price);
-    
+
     if ($show_publisher) {
       $to_return .= $this->generateBookInfoLine('Publisher', $this->publisher);
     }
-    
+
     if ($showISBN) {
       $to_return .= $this->generateBookInfoLine('ISBN', $this->isbn);
     }
-    
+
     return $to_return;
   }
-  
+
   /**
    * Generate line in book info content
    *
@@ -73,20 +73,33 @@ class Book {
     return '<div class="book-info book-' . $lower_case . '">
           <label class="book-label">' . $label . '</label>' . $content . '</div>';
   }
-  
+
   /**
    * Generate content for a book with cover, used on the main page
    */
   public function generateBookView() {
     $to_return = '<div class="book-view">
-          <img src="' . $this->cover . '" class="book-cover" /><div class="book-info-box">'
+          <img src="' . $this->get_cover($this->cover) . '" class="book-cover" /><div class="book-info-box">'
             . $this->generateBookInfoLine('Title', $this->title)
             . $this->generateBookInfoLine('Author', $this->first_name . ' ' . $this->last_name)
             . '</div>
              </div>';
     return $to_return;
   }
-  
+
+  private function get_cover($cover) {
+    if ($cover) {
+      return 'admin/images/covers/' . $cover;
+    } else {
+      //include_once 'constants.php';
+      //$images = glob($GLOBALS['locations']['random_covers'] . '*.{jpg,jpeg,png,gif}', GLOB_BRACE);
+      $images = scandir('admin/images/covers/random/');
+      do {
+        $the_image = $images[array_rand($images)];
+      } while (substr($the_image, 0, 1) === '.');
+      return 'admin/images/covers/random/' . $the_image;
+    }
+  }
 }
 
 ?>
