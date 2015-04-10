506782dfc3652c32cb9823b8c8b7d135b681fff8
Eddie Gurnee <pegurnee@gmail.com>
2015-04-10 09:44:54 -0400

    fix register to input street address

--- api/register.php
+++ api/register.php
@@ -22,31 +22,35 @@ if (!$updating && $customer_inserter->does_exist($_POST['username'])) {
 } else {
 
   try {
-    
+
     if ($updating && $_POST['address-selection'] == 'other-address') {
       $address_id = intval($_POST['other-address']);
     } elseif ($updating && $_POST['address-selection'] == 'current-address') {
       $address_id = $login->get_primary_address()['id'];
     } else {
       $address_info = [
-        'street_address' => $_POST['street_address'],
+        'street_address' => $_POST['street-address'],
         'city' => $_POST['city'],
         'zip' => $_POST['zip'],
         'state' => $_POST['state']
       ];
-      
+
       $address_id = $address_inserter->insert($address_info)['id'];
     }
-    
+
     if ($updating) {
-      $customer_inserter->update([
+      $update = [
         'username' => $_POST['username'],
-        'password' => $_POST['password'],
         'email' => $_POST['email'],
         'first_name' => $_POST['first-name'],
         'last_name' => $_POST['last-name'],
         'address_id' => $address_id
-      ]);
+      ];
+      if (isset($_POST['password'])) {
+        $update['password'] = $_POST['password'];
+      }
+
+      $customer_inserter->update($update);
     } else {
       $customer_inserter->insert([
         'username' => $_POST['username'],
@@ -57,14 +61,14 @@ if (!$updating && $customer_inserter->does_exist($_POST['username'])) {
         'address_id' => $address_id
       ]);
     }
-  
+
     $card_id = $card_inserter->insert([
       'username' => $_POST['username'],
       'number' => $_POST['card-number'],
       'issuer' => $_POST['card-type'],
       'expiration' => $_POST['card-expiration']
     ]);
-  
+
     try {
       // insert customer, address into lookup table
       $query = 'INSERT INTO customer_address
@@ -77,7 +81,7 @@ if (!$updating && $customer_inserter->does_exist($_POST['username'])) {
     } catch (PDOException $e) {
       // exception thrown if username/address combo already in
     }
-  
+
     // put card number into customer
     // TODO: do this when initially adding customer
     $query = 'UPDATE customer
@@ -91,7 +95,7 @@ if (!$updating && $customer_inserter->does_exist($_POST['username'])) {
   } catch (PDOException $e) {
     print_r($e);
   }
-  
+
   $success = true;
 }
 
